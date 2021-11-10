023cccfe0e538aa07b6779ae0e0446c7a67e4bda
Luca Guerra <luca.guerra@sysdig.com>
2021-11-10 12:09:47 +0000

    update falcosecurity/libs commit for sysdig

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -29,8 +29,8 @@ else()
   # default below In case you want to test against another falcosecurity/libs version just pass the variable - ie., `cmake
   # -DFALCOSECURITY_LIBS_VERSION=dev ..`
   if(NOT FALCOSECURITY_LIBS_VERSION)
-    set(FALCOSECURITY_LIBS_VERSION "f7029e2522cc4c81841817abeeeaa515ed944b6c")
-    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=63e602c05db142465211e2d151d0ccd08fdb613fe85dd3603c8298bc0108823a")
+    set(FALCOSECURITY_LIBS_VERSION "fcffb427b75569706e83b11c62cc7673de38aab9")
+    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=2b92aacc4af766146bda4166a4d6c082ab9bdb1658ad8ff143bdf33d390921a0")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
