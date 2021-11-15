a2a68e15a2543c2ab88495acc142f0b58ecd0fc3
Luca Guerra <luca.guerra@sysdig.com>
2021-11-15 18:34:34 +0000

    add s3 upload

--- scripts/aws-test/do_test.py
+++ scripts/aws-test/do_test.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+import io
+
 import boto3
 
 # change 6
 
 def main():
-    print(f"aws test script. boto3 version: {boto3.__version__}")
+    print(f"[*] aws test script. boto3 version: {boto3.__version__}")
+
+    s3 = boto3.client('s3')
+
+    bio = io.BytesIO()
+    bio.write(b'Hello s3')
+    bio.seek(0)
+
+    s3.upload_fileobj(bio, "lucag-test-personal", "hello_s3/hello.bin")
+    print("[*] test upload completed.")
 
 if __name__ == '__main__':
     sys.exit(main())
