9f57ff0291e831507dea8a760371686df26a212b
Luca Guerra <luca.guerra@sysdig.com>
2021-11-15 16:58:25 +0000

    trigger aws test action

---
+++ scripts/aws-test/do_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+import boto3
+
+def main():
+    print(f"aws test script. boto3 version: {boto3.__version__}")
+
+if __name__ == '__main__':
+    sys.exit(main())
