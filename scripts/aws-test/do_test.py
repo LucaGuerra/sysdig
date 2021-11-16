d90076a294b4747f81df887e3bf856083b3653e5
Luca Guerra <luca.guerra@sysdig.com>
2021-11-16 10:13:36 +0000

    check key exist

--- scripts/aws-test/do_test.py
+++ scripts/aws-test/do_test.py
@@ -4,13 +4,28 @@ import sys
 import io
 
 import boto3
+from botocore.errorfactory import ClientError
 
-# change 6
+# change 7
+
+def s3_exists(s3, bucket: str, key: str) -> bool:
+    try:
+        s3.head_object(Bucket=bucket, Key=bucket)
+    except ClientError:
+        # Not found
+        return False
+
+    return True
 
 def main():
     print(f"[*] aws test script. boto3 version: {boto3.__version__}")
 
     s3 = boto3.client('s3')
+
+    if s3_exists("lucag-test-personal", "hello_s3/hello.bin"):
+        print("[*] key already exists on S3")
+    else:
+        print("[*] key does not exist on s3")
 
     bio = io.BytesIO()
     bio.write(b'Hello s3')
