14e5c44e03147879e2028237a19a28158415193a
Luca Guerra <luca.guerra@sysdig.com>
2021-11-16 20:05:27 +0000

    add s3 to build script

--- scripts/driverkit/build/build.py
+++ scripts/driverkit/build/build.py
@@ -3,9 +3,9 @@
 import sys
 import os
 import yaml
+import boto3
 import shutil
 import argparse
-import pathlib
 import subprocess
 from pathlib import Path
 
@@ -22,6 +22,21 @@ def driverkit_build(driverkit: str, config_file: Path, driverversion: str, devic
     status = subprocess.run(args)
 
     return status.returncode == 0
+
+def s3_exists(s3, bucket: str, key: str) -> bool:
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+    except ClientError:
+        # Not found
+        return False
+
+    return True
+
+def delete_file(filename: str):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
 
 def main():
     ap = argparse.ArgumentParser()
@@ -48,6 +63,14 @@ def main():
         print(f"[-] driverkit binary {driverkit} does not exist")
         return 1
 
+    s3 = None
+    s3_bucket = None
+    s3_prefix = None
+    if args.s3_bucket is not None and args.s3_prefix is not None:
+        s3 = boto3.client('s3')
+        s3_bucket = args.s3_bucket
+        s3_prefix = args.s3_prefix.lstrip('/')
+
     dri_dirs = [x for x in config_dir.iterdir() if x.is_dir()]
     for dri_dir in dri_dirs:
         driverversion = dri_dir.name
@@ -58,6 +81,7 @@ def main():
         count = 0
         success_count = 0
         fail_count = 0
+        skip_count = 0
         for config_file in files:
             count += 1
             print('[*] [{:03d}/{:03d}] {}'.format(count, len(files), config_file.name))
@@ -68,12 +92,22 @@ def main():
             module_output = conf.get('output', {}).get('module')
             probe_output = conf.get('output', {}).get('probe')
             
-            # this needs to change if we have s3 or not
-            need_module = (module_output is not None) and not os.path.exists(module_output)
-            need_probe = (probe_output is not None) and not os.path.exists(probe_output)
+            module_basename = os.path.basename(module_output)
+            probe_basename = os.path.basename(probe_output)
+
+            module_s3key = f"{s3_prefix}/{driverversion}/{module_basename}"
+            probe_s3key = f"{s3_prefix}/{driverversion}/{probe_basename}"
+
+            if s3:
+                need_module = (module_output is not None) and not s3_exists(s3, s3_bucket, module_s3key)
+                need_probe = (probe_output is not None) and not s3_exists(s3, s3_bucket, probe_s3key)
+            else:
+                need_module = (module_output is not None) and not os.path.exists(module_output)
+                need_probe = (probe_output is not None) and not os.path.exists(probe_output)
 
             need_build = need_module or need_probe
             if not need_build:
+                skip_count += 1
                 print('[*] {} already built'.format(config_file))
                 continue
 
@@ -83,15 +117,27 @@ def main():
             if probe_output is not None:
                 Path(probe_output).parent.mkdir(parents=True, exist_ok=True)
 
-            success = driverkit_build(driverkit, config_file, driverversion, "sysdig", "sysdig-probe")
+            success = driverkit_build(driverkit, config_file, driverversion, 'sysdig', 'sysdig-probe')
             if success:
-                print('[+] Build completed {}'.format(config_file))
+                print(f"[+] Build completed {config_file}")
                 success_count += 1
             else:
-                print('[-] Build failed    {}'.format(config_file))
+                print(f"[-] Build failed {config_file}")
                 fail_count += 1
 
-        print(f"[*] Build {driverversion} complete. {success_count}/{count} built, {fail_count}/{count} failed.")
+            # upload to s3 and remove
+            if s3:
+                if module_output is not None:
+                    with open(module_output, 'rb') as fp:
+                        s3.upload_fileobj(fp, s3_bucket, module_s3key)
+                    delete_file(module_output)
+
+                if probe_output is not None:
+                    with open(probe_output, 'rb') as fp:
+                        s3.upload_fileobj(fp, s3_bucket, probe_s3key)
+                    delete_file(probe_output)
+
+        print(f"[*] Build {driverversion} complete. {success_count}/{count} built, {fail_count}/{count} failed, {skip_count}/{count} already built.")
 
     return 0
 
