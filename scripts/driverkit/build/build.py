902781a4a8f6f6f36f5840a1cece83a2c431b1c7
Luca Guerra <luca.guerra@sysdig.com>
2021-11-16 15:53:49 +0000

    change 2

--- scripts/driverkit/build/build.py
+++ scripts/driverkit/build/build.py
@@ -9,7 +9,7 @@ import pathlib
 import subprocess
 from pathlib import Path
 
-# change 1
+# change 2
 
 def driverkit_build(driverkit: str, config_file: Path, driverversion: str, devicename: str, drivername: str) -> bool:
     args = [driverkit, 'docker',
