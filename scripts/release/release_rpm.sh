2c5c1f5c7bca85abc083ee0b491cc3230bdcf6f2
Luca Guerra <luca.guerra@sysdig.com>
2021-11-20 18:30:08 +0000

    remove public acl permissions

--- scripts/release/release_rpm.sh
+++ scripts/release/release_rpm.sh
@@ -14,7 +14,9 @@ echo "S3_BUCKET_NAME: $S3_BUCKET_NAME"
 
 mkdir -p $REPOSITORY_DIR/rpm/$RPM_BASEARCH
 
-aws s3 sync s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ --acl public-read --exact-timestamps
+# NOTE: the ACLs for public reading are removed until we use a test private bucket. Will need to be enabled for production
+
+aws s3 sync s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ --exact-timestamps # --acl public-read 
 # ls -1tdr $REPOSITORY_DIR/rpm/$RPM_BASEARCH/*sysdig*.rpm | head -n -5 | xargs -d '\n' rm -f || true
 
 cp $PACKAGES_DIR/*rpm $REPOSITORY_DIR/rpm/$RPM_BASEARCH
@@ -23,5 +25,5 @@ createrepo $REPOSITORY_DIR/rpm/$RPM_BASEARCH
 cp $SCRIPTS_DIR/draios.repo $REPOSITORY_DIR/rpm
 sed -i s/_REPOSITORY_/$REPOSITORY_NAME/g $REPOSITORY_DIR/rpm/draios.repo
 
-aws s3 cp $REPOSITORY_DIR/rpm/draios.repo s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/ --acl public-read
-aws s3 sync $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ --acl public-read --exact-timestamps
+aws s3 cp $REPOSITORY_DIR/rpm/draios.repo s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/ # --acl public-read
+aws s3 sync $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ --exact-timestamps # --acl public-read 
