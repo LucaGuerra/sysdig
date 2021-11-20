b7e3d4b8d2ac8b648dcbb6c01bf54b6adbe48499
Luca Guerra <luca.guerra@sysdig.com>
2021-11-20 16:26:18 +0000

    add RPM release script

---
+++ scripts/release/release_rpm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# required env variables
+echo "REPOSITORY_DIR: $REPOSITORY_DIR"
+echo "RPM_BASEARCH: $RPM_BASEARCH"
+echo "REPOSITORY_NAME: $REPOSITORY_NAME"
+echo "PACKAGES_DIR: $PACKAGES_DIR"
+echo "SCRIPTS_DIR: $SCRIPTS_DIR"
+echo "S3_BUCKET_NAME: $S3_BUCKET_NAME"
+
+mkdir -p $REPOSITORY_DIR/rpm/$RPM_BASEARCH
+
+aws s3 sync s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ --acl public-read --exact-timestamps
+# ls -1tdr $REPOSITORY_DIR/rpm/$RPM_BASEARCH/*sysdig*.rpm | head -n -5 | xargs -d '\n' rm -f || true
+
+cp $PACKAGES_DIR/*rpm $REPOSITORY_DIR/rpm/$RPM_BASEARCH
+createrepo $REPOSITORY_DIR/rpm/$RPM_BASEARCH
+
+cp $SCRIPTS_DIR/draios.repo $REPOSITORY_DIR/rpm
+sed -i s/_REPOSITORY_/$REPOSITORY_NAME/g $REPOSITORY_DIR/rpm/draios.repo
+
+aws s3 cp $REPOSITORY_DIR/rpm/draios.repo s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/ --acl public-read
+aws s3 sync $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ --acl public-read --exact-timestamps
