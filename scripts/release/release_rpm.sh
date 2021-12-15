84bd328695fda671ef87322b3eca79755c10ebc4
Luca Guerra <luca.guerra@sysdig.com>
2021-12-15 10:54:18 +0000

    update release

--- scripts/release/release_rpm.sh
+++ scripts/release/release_rpm.sh
@@ -8,13 +8,11 @@ echo "RPM_BASEARCH: $RPM_BASEARCH"
 echo "REPOSITORY_NAME: $REPOSITORY_NAME"
 echo "PACKAGES_DIR: $PACKAGES_DIR"
 echo "SCRIPTS_DIR: $SCRIPTS_DIR"
-echo "S3_BUCKET_NAME: $S3_BUCKET_NAME"
+echo "S3_BUCKET: $S3_BUCKET"
 
 mkdir -p $REPOSITORY_DIR/rpm/$RPM_BASEARCH
 
-# NOTE: the ACLs for public reading are removed until we use a test private bucket. Will need to be enabled for production
-
-aws s3 sync s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ --exact-timestamps --acl public-read # --delete
+aws s3 sync s3://$S3_BUCKET/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ --exact-timestamps --acl public-read # --delete
 # ls -1tdr $REPOSITORY_DIR/rpm/$RPM_BASEARCH/*sysdig*.rpm | head -n -5 | xargs -d '\n' rm -f || true
 
 cp $PACKAGES_DIR/*rpm $REPOSITORY_DIR/rpm/$RPM_BASEARCH
@@ -23,5 +21,5 @@ createrepo $REPOSITORY_DIR/rpm/$RPM_BASEARCH
 cp $SCRIPTS_DIR/draios.repo $REPOSITORY_DIR/rpm
 sed -i s/_REPOSITORY_/$REPOSITORY_NAME/g $REPOSITORY_DIR/rpm/draios.repo
 
-aws s3 cp $REPOSITORY_DIR/rpm/draios.repo s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/ --acl public-read # --delete
-aws s3 sync $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ s3://$S3_BUCKET_NAME/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ --exact-timestamps --acl public-read # --delete
+aws s3 cp $REPOSITORY_DIR/rpm/draios.repo s3://$S3_BUCKET/$REPOSITORY_NAME/rpm/ --acl public-read # --delete
+aws s3 sync $REPOSITORY_DIR/rpm/$RPM_BASEARCH/ s3://$S3_BUCKET/$REPOSITORY_NAME/rpm/$RPM_BASEARCH/ --exact-timestamps --acl public-read --delete
